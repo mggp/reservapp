d6fb87db1fc0803d0c4933e7ce78789f55d5cdd2
Matías G. González Pazos <matiasggonzalez42@gmail.com>
2022-02-16 18:03:22 +0100

    Agrego vista para mostrar lista de reservas

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking, Room, RoomType
 
-# Create your views here.
+def booking_details(request, booking_code):
+    pass
+
+class BookingDetailsView(generic.DetailView):
+    model = Booking
+
+def booking_form(request):
+    pass
+
+def booking_list(request):
+    pass
+
+class BookingListView(generic.ListView):
+    model = Booking
+
+    def get_queryset(self):
+        return Booking.objects.all()
+
+    def get_context_data(self, **kwargs):
+        ctxt = super().get_context_data(**kwargs)
+        ctxt['nav_item_selected'] = 'Booking'
+        return ctxt
+
+def search_for_available_rooms(request): # TODO: This name is not quite right, change it
+    pass
+
