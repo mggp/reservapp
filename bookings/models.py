1ec58620328452a42584d65a2bd35343effb3346
Matías G. González Pazos <matiasggonzalez42@gmail.com>
2022-02-16 11:13:22 +0100

    Agrego métodos a modelos para mostrarlos como strings

--- bookings/models.py
+++ bookings/models.py
@@ -36,17 +36,23 @@ class ContactDetailsField(models.TextField):
         return json.dumps(obj)
 
 
-# TODO: implement __str__
+# TODO: add field descriptions?
 class RoomType(models.Model):
     capacity = models.PositiveSmallIntegerField()
     daily_rate = models.DecimalField(decimal_places = 2, max_digits = 20)
     name = models.CharField(max_length = 255)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Room(models.Model):
     room_type = models.ForeignKey(RoomType, on_delete = models.CASCADE)
     # It's a name and not a number in case there's a
     #  situation in which a name like '303bis' or sth is required  
-    name = models.CharField(max_length = 255)    
+    name = models.CharField(max_length = 255)
+
+    def __str__(self) -> str:
+        return " ".join((self.name, "("+str(self.room_type)+")"))
 
 class Booking(models.Model):
     start_date = models.DateField()
@@ -60,5 +66,12 @@ class Booking(models.Model):
     # TODO: tidy/refactor contact details. Could also be a collection?
     contact_info = ContactDetailsField()
     room = models.ForeignKey(Room, on_delete = models.SET_NULL, null = True)
+
+    def __str__(self) -> str:
+        return " ".join((
+            self.code, 
+            self.room.name, 
+            "("+str(self.guest_count)+" PAX)", 
+            "Check in:", str(self.start_date)))
 
 
